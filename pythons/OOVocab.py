eb93cca81b9d4e076ab89860c7020b5c8f8b2b7d
vairylein <emmawbv@hotmail.com>
2014-07-26 18:07:36 +0200

    Iron Sky

---
+++ pythons/OOVocab.py
@@ -0,0 +1,24 @@
+import os
+
+
+def firstunk(doc,name):
+
+	with open(doc,"r") as myfile:
+		txt = myfile.readlines()
+
+	f = open(name, "w")
+	d = {}
+
+	for line in txt:
+		words = line.split(" ")[:-1]
+		sentence =[]
+		for word in words:
+			
+			if word in d.keys():
+				sentence+= [word]
+			else:
+				sentence+= ["<unk>"]
+				d[word] = 0
+		f.write(" ".join(sentence) + "\n")
+
+	f.close()
